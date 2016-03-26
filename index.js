010512dc80d899767b122f9ebe1aaddf98730dd1
Samet Aylak <sametaylak29@gmail.com>
2016-03-26 18:38:38 +0200

    ExpressJS & NodeJS

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app     = express();
+    
+    
+const port = 3000;
+    
+app.get('/', function (req, res) {
+  console.log('Bir istek geldi!');
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/hakkimizda', function(req, res) {
+  res.sendFile(__dirname + '/about.html');
+})
+
+app.listen(port, function () {
+  console.log('Sunucu %s portunu dinliyor!', port);
+});
